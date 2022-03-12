b658adf83738e757ea98d3160444967fe53e2295
nikmall <nikolaosmallios@outlook.com>
2022-03-12 14:59:03 +0200

    added model training and project configuration

---
+++ config.py
@@ -0,0 +1,7 @@
+bert_conf = {
+    "max_len": 150,
+    "bert_pre_trained": "bert-base-uncased",
+    "bert_folder": "bert_base_uncased/",
+    "train_path": "inputs/train-v1.1.json",
+    "dev_path": "inputs/dev-v1.1.json",
+}
