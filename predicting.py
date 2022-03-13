3784ce9b4d9e40544a160f8e949827f985816d83
nikmall <nikolaosmallios@outlook.com>
2022-03-13 18:01:50 +0200

    Updating and fixes in predicting

--- predicting.py
+++ predicting.py
@@ -1,25 +1,34 @@
-from tensorflow.keras.models import load_model
 import numpy as np
-from tokenizers import BertWordPieceTokenizer
-
 import config
-from utils.data_utils import ApiQuestion, create_api_inputs, create_api_questions
+
+from tensorflow.keras.models import load_model
+
+from utils.data_utils import create_api_inputs, create_api_questions, create_squad_qas, create_inputs_targets
 from utils.model_utils import load_bert_tokenizer
 
 
 def predict_api(test_dict):
     conf = config.bert_conf
     tokenizer = load_bert_tokenizer(conf["bert_folder"])
-
-    text_question_list = create_api_questions(test_dict, tokenizer, conf["max_len"])
+    # create data
+    data_dict = test_dict.dict()
+    text_question_list = create_api_questions(data_dict, tokenizer, conf["max_len"])
     x_test = create_api_inputs(text_question_list)
     model = load_model('model')
 
-    pred_start, pred_end = model.predict(x_test)
+    # in case of empty list after processing(due to max length filtering)
+    if x_test[-1].shape[0] == 0:
+        return []
 
+    pred_start, pred_end = model.predict(x_test)
+    answers = get_pred_answer_text(pred_start, pred_end, text_question_list)
+    return answers
+
+
+def get_pred_answer_text(pred_start, pred_end, questions_list):
     answers = []
     for i, (start, end) in enumerate(zip(pred_start, pred_end)):
-        api_question = text_question_list[i]
+        api_question = questions_list[i]
         offsets = api_question.context_token_to_char
         start = np.argmax(start)
         end = np.argmax(end)
@@ -33,5 +42,4 @@ def predict_api(test_dict):
         else:
             pred_answer = api_question.context[pred_char_start:]
         answers.append(pred_answer)
-
-    print(answers)
+    return answers
