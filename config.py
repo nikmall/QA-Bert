ce2a2a656af1b9dfd1e38cabe186fa128698d223
nikmall <nikolaosmallios@outlook.com>
2022-03-14 00:19:19 +0200

    Fix on lr

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ bert_conf = {
     "bert_folder": "bert_base_uncased/",
     "train_path": "inputs/train-v1.1.json",
     "dev_path": "inputs/dev-v1.1.json",
-    "learning_rate": 0.0001,
+    "learning_rate": 0.00001,
     "batch": 32
 }
